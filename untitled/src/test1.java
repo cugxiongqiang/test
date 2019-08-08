62ab1c6bbbd783ad587f596fa3c2315be695dc5d
cugxiongqiang <xiongqiang@cug.edu.cn>
2019-08-08 09:42:49 +0800

    kaifa0808

--- untitled/src/test1.java
+++ untitled/src/test1.java
@@ -2,7 +2,11 @@
  * Created by DELL on 2019/8/8.
  */
 public class test1 {
+    public final static int i=0;
     public void t() {
+        System.out.println();
+        System.out.println();
+        System.out.println();
         System.out.println();
     }
 
